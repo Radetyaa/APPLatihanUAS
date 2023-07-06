d26e13e95c1403300d00761a31a319bfd5b0234d
Radetyaa <bimaadit240100@gmail.com>
2023-07-06 12:12:09 +0700

    UAS finish 6 jul 2023

--- application/views/tampil.blade.php
+++ application/views/tampil.blade.php
@@ -1,4 +1,3 @@
-
 <!doctype html>
 <html lang="en" data-bs-theme="auto">
   <head><script src="/docs/5.3/assets/js/color-modes.js"></script>
@@ -39,18 +38,25 @@
         <table class="table table-bordered">
           <tr>
             <th>Username</th>
+            <th>Email</th>
             <th>Artikel</th>
+            <th>Jenis</th>
             <th>Created_at</th>
-            <th>Update_at</th>
-            <th>Aksi</th>
+            <th>Updated_at</th>
+            <th>Action</th>
           </tr>
           @foreach($post_list as $post)
           <tr>
             <td>{{ $post->user->username }}</td>
+            <td>{{ $post->user->email }}</td>
             <td>{{ $post->artikel }}</td>
+            <td>{{ $post->jenis }}</td>
             <td>{{ $post->created_at }}</td>
             <td>{{ $post->updated_at }}</td>
-            <td><a href="{{ site_url('Welcome/hapus/'.$post->id)}}">Hapus</a> | <a href="{{ site_url('Welcome/ubah/'.$post->id) }}">Ubah</a></td>
+            <td>
+              <a href="{{ site_url('Welcome/hapus/'.$post->id)}}" class="btn btn-danger">Delete</a> 
+              <a href="{{ site_url('Welcome/ubah/'.$post->id) }}" class="btn btn-info">Edit</a>
+            </td>
           </tr>
           @endforeach
         </table>
